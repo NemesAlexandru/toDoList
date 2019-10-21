e35c972807fd214e24ac8c3f461e45f80a08c19d
NemesAlexandru <alexandrunemes1@gmail.com>
2019-10-21 20:05:29 +0300

    Added update, delete and get to-do-items methods in repository.

--- src/main/java/org/fasttrackit/todolist/App.java
+++ src/main/java/org/fasttrackit/todolist/App.java
@@ -1,11 +1,13 @@
 package org.fasttrackit.todolist;
 
+import org.fasttrackit.todolist.domain.ToDoItem;
 import org.fasttrackit.todolist.persistance.ToDoItemRepository;
 import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
@@ -15,6 +17,13 @@ public class App {
         request.setDeadline(LocalDate.now().plusWeeks(1));
 
         ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
-        toDoItemRepository.createToDoItem(request);
+//        toDoItemRepository.createToDoItem(request);
+//        toDoItemRepository.updateToDoItem(1, true);
+//        toDoItemRepository.deleteTodoItem(1);
+
+        List<ToDoItem> toDoItems = toDoItemRepository.getToDoItems();
+
+        System.out.println(toDoItems);
+
     }
 }
