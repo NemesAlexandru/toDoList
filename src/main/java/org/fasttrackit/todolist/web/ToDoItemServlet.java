6ec519baa09fd400f4fbeddc4c82593b48f58764
NemesAlexandru <alexandrunemes1@gmail.com>
2019-10-28 21:16:09 +0200

    Refactor object mapper

--- src/main/java/org/fasttrackit/todolist/web/ToDoItemServlet.java
+++ src/main/java/org/fasttrackit/todolist/web/ToDoItemServlet.java
@@ -2,6 +2,7 @@ package org.fasttrackit.todolist.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fasttrackit.todolist.config.ObjectMapperConfiguration;
 import org.fasttrackit.todolist.domain.ToDoItem;
 import org.fasttrackit.todolist.service.ToDoItemService;
 import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
@@ -25,9 +26,10 @@ public class ToDoItemServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        CreateToDoItemRequest request = objectMapper.readValue(req.getReader(), CreateToDoItemRequest.class);
-        objectMapper.registerModule(new JavaTimeModule());
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
+
+        CreateToDoItemRequest request = ObjectMapperConfiguration.getObjectMapper().readValue(req.getReader(), CreateToDoItemRequest.class);
 
         try {
             toDoItemService.createToDoItem(request);
@@ -53,10 +55,10 @@ public class ToDoItemServlet extends HttpServlet {
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String id = req.getParameter("id");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
 
-        UpdateToDoItemRequest request = objectMapper.readValue(req.getReader(), UpdateToDoItemRequest.class);
+        UpdateToDoItemRequest request = ObjectMapperConfiguration.getObjectMapper().readValue(req.getReader(), UpdateToDoItemRequest.class);
 
         try {
             toDoItemService.updateToDoItem(Long.parseLong(id), request);
@@ -69,9 +71,9 @@ public class ToDoItemServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             List<ToDoItem> toDoItems = toDoItemService.getToDoItems();
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule());
-            String response = objectMapper.writeValueAsString(toDoItems);
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            objectMapper.registerModule(new JavaTimeModule());
+            String response = ObjectMapperConfiguration.getObjectMapper().writeValueAsString(toDoItems);
 
             resp.getWriter().println(response);
         }
